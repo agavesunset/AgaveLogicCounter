5dec7ccc6ba2481921fdfce458a46bcf0d2b1328
Agave Sunset <201904066+agavesunset@users.noreply.github.com>
2025-12-18 14:15:15 +0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-# __init__.py 放在 AgaveLogicCounter 文件夹里
-
-from .cyclic_int_controller import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
